f3767b11d8a78eb865f07e2f344c3ac9018b995b
Thai Chi Thanh <thanhchithai2005@gmail.com>
2026-05-16 10:42:10 +0700

    update MapStruct configuration, enhance Elasticsearch SSL connection, and update Helm chart dependencies

--- search/src/main/java/com/yas/search/config/ImperativeClientConfig.java
+++ search/src/main/java/com/yas/search/config/ImperativeClientConfig.java
@@ -7,6 +7,11 @@ import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.yas.search.repository")
 @ComponentScan(basePackages = "com.yas.search.service")
@@ -17,14 +22,32 @@ public class ImperativeClientConfig extends ElasticsearchConfiguration {
 
     @Override
     public ClientConfiguration clientConfiguration() {
-        String url = elasticsearchConfig.getUrl()
+        String originalUrl = elasticsearchConfig.getUrl();
+        boolean useSSL = originalUrl.startsWith("https://");
+        String host = originalUrl
             .replace("https://", "")
             .replace("http://", "");
-    
-        return ClientConfiguration.builder()
-                .connectedTo(url)
-                .usingSsl()
-                .withBasicAuth(elasticsearchConfig.getUsername(), 
+
+        ClientConfiguration.MaybeSecureClientConfigurationBuilder builder =
+            ClientConfiguration.builder().connectedTo(host);
+
+        if (useSSL) {
+            try {
+                // Trust all certificates (disable verification for dev/self-signed certs)
+                SSLContext sslContext = SSLContext.getInstance("TLS");
+                sslContext.init(null, new TrustManager[]{new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                }}, null);
+                builder = builder.usingSsl(sslContext);
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to create SSL context", e);
+            }
+        }
+
+        return builder
+                .withBasicAuth(elasticsearchConfig.getUsername(),
                             elasticsearchConfig.getPassword())
                 .build();
     }
